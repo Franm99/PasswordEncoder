f0d9dc765c02774c42037ed1c7fe67e3a9e7ee8d
Franm99 <fran.moreno.se@gmail.com>
2024-09-17 08:35:20 +0200

    Added second method and Decoding

--- src.js
+++ src.js
@@ -1,9 +1,13 @@
-/*
-- Encryption method 1:
-    1. Shift string characters
-    2. Encode
-*/
+NUM_SHIFTS = 4
 
+/**
+ * This function computes a circular shift on a string given a number of positions.
+ * 
+ * @param {String} string 
+ * @param {Number} num 
+ * @param {String} direction 
+ * @returns 
+ */
 function shiftString(string, num = null, direction = "left") {
     if (num === null) {
         num = Math.floor(string.length / 2);
@@ -18,15 +22,71 @@ function shiftString(string, num = null, direction = "left") {
     }
 }
 
-NUM_SHIFTS = 4
+
+/**
+ * This function rearranges a string by parity, separating odd and even numbers and concatenating the sub-strings.
+ * 
+ * @param {String} string 
+ * @param {Boolean} reverse 
+ * @returns 
+ */
+function rearrangeByParity(string, reverse=false) {
+    if (reverse == false) {
+        let even_chars = "";
+        let odd_chars = "";
+        for (let i = 0; i < string.length; i++) {
+            if (i % 2 == 0) {
+                even_chars += string[i];
+            } else {
+                odd_chars += string[i];
+            }
+        }
+        return even_chars + odd_chars;
+    } else {
+        let half = Math.ceil(string.length / 2);
+        let first_part = string.substring(0, half);
+        let second_part = string.substring(half);
+
+        let new_str = ""
+        for (let i = 0; i < first_part.length; i++) {
+            new_str += first_part[i];
+            if (i < second_part.length) { new_str += second_part[i]; }
+        }
+        return new_str
+    }
+}
+
 
 window.addEventListener("DOMContentLoaded", function() {
 
-    document.getElementById("button-submit").addEventListener("click", function() {
-        pwd = document.getElementById("get-input").value;
-        pwdShifted = shiftString(pwd, NUM_SHIFTS);
-        pwdShiftedEncoded = btoa(pwdShifted);
+    document.getElementById("encode").addEventListener("click", function() {
+        let pwd = document.getElementById("get-input").value;
+        let method = document.getElementById("encryption-type").value;
+        var pwdShifted = ""
+        
+        if (method == "method-1") {
+            pwdShifted = shiftString(pwd, NUM_SHIFTS);
+        } else {
+            pwdShifted = rearrangeByParity(pwd);
+            console.log(pwdShifted);
+        }
 
+        let pwdShiftedEncoded = btoa(pwdShifted);
         document.getElementById("output-text").innerText = pwdShiftedEncoded;
+    })
+
+    document.getElementById("decode").addEventListener("click", function() {
+        let encoded = document.getElementById("get-input").value;
+        let method = document.getElementById("encryption-type").value;
+
+        let pwdShifted = atob(encoded);
+
+        if (method == "method-1") {
+            pwd = shiftString(pwdShifted, NUM_SHIFTS, "right");
+        } else {
+            pwd = rearrangeByParity(pwdShifted, reverse=true);
+        }
+
+        document.getElementById("output-text").innerText = pwd;
     })
 })
